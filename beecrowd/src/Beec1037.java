08293582661dbd405c490dd38ad4d1ca223c56e6
Rufat <165446747+rufat200@users.noreply.github.com>
2025-09-30 08:16:46 +0600

    some commit2

---
+++ beecrowd/src/Beec1037.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Beec1037 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        s.useLocale(java.util.Locale.US);
+        double n = s.nextDouble();
+        String str = "Intervalo ";
+        if(n<0 || n>100) {
+            System.out.println("Fora de intervalo");
+        } else if(n>=0 && n <=25) {
+            System.out.println(str+"[0,25]");
+        } else if(n>25 && n<=50) {
+            System.out.println(str+"(25,50]");
+        } else if(n>50 && n<=75) {
+            System.out.println(str+"(50,75]");
+        } else if(n>75 && n<=100){
+            System.out.println(str+"(75,100]");
+        }
+    }
+}
